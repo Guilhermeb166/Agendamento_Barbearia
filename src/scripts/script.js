66189fc451ed326401f354361c41633bc4e35a41
Guilhermeb166 <guilhermebarroso166@gmail.com>
2024-06-05 15:35:53 -0300

    adicionado botão para agendar, adicionado função de abrir e fechar os horários, adicionada cor dourada,troca de logo

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,23 +1,3 @@
-function borda(event) {
-    let opcao = event.currentTarget.parentNode; // Captura o elemento pai do input (ou seja, o label)
-
-    // Verifica se o elemento clicado já possui a classe 'borda-adicionada'
-    if (opcao.classList.contains('borda-adicionada')) {
-        opcao.classList.remove('borda-adicionada'); // Remove a classe se já estiver presente
-    } else {
-        let labels = document.querySelectorAll('#service-option label');
-
-        // Remove a classe 'borda-adicionada' de todos os labels
-        labels.forEach(label => {
-            if (label.classList.contains('borda-adicionada')) {
-                label.classList.remove('borda-adicionada');
-            }
-        });
-
-        // Adiciona a classe 'borda-adicionada' ao elemento clicado
-        opcao.classList.add('borda-adicionada');
-    }
-}
 
 document.addEventListener('DOMContentLoaded', function () {
     const dataInput = document.getElementById('data');
@@ -59,3 +39,70 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+//Função para mostrar as horas disponíveis 
+function abrirHorarios(){
+    let horas = document.getElementById("horarios")
+    horas.style.display="grid"
+    document.querySelector(".form p").style.display="none"
+}
+
+//Função para fechar as horas disponiveís
+function fechaHorarios(){
+    let horas = document.getElementById("horarios")
+    horas.style.display='none'
+    document.querySelector(".form p").style.display="block"
+}
+
+/*Função para adicionar cor de fundo ao selecionar a hora desejada */
+function hora(event) {
+    let opcao = event.currentTarget.parentNode; // Captura o elemento pai do input (ou seja, o label)
+  
+    // Verifica se o elemento clicado já possui a classe 'background-add'
+    if (opcao.classList.contains('background-add')) {
+      opcao.classList.remove('background-add'); // Remove a classe se já estiver presente
+      
+    } else {
+      
+      let labels = document.querySelectorAll('#horarios label');
+    
+      // Remove a classe 'background-add' de todos os labels
+      labels.forEach(label => {
+        if (label.classList.contains('background-add')) {
+        
+          label.classList.remove('background-add');
+        }
+      });
+  
+      // Adiciona a classe 'background-add' ao elemento clicado
+      opcao.classList.add('background-add');
+    
+    }
+
+  }
+
+  /*Função para adicionar borda ao selecionar o tipo de serviço */
+function borda(event) {
+    let opcao = event.currentTarget.parentNode; // Captura o elemento pai do input (ou seja, o label)
+
+    // Verifica se o elemento clicado já possui a classe 'borda-adicionada'
+    if (opcao.classList.contains('borda-adicionada')) {
+        opcao.classList.remove('borda-adicionada'); // Remove a classe se já estiver presente
+    } else {
+        let labels = document.querySelectorAll('#service-option label');
+
+        // Remove a classe 'borda-adicionada' de todos os labels
+        labels.forEach(label => {
+            if (label.classList.contains('borda-adicionada')) {
+                label.classList.remove('borda-adicionada');
+            }
+        });
+
+        // Adiciona a classe 'borda-adicionada' ao elemento clicado
+        opcao.classList.add('borda-adicionada');
+    }
+}
+
+  
+
+
